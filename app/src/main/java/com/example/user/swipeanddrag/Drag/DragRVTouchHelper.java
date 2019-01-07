aa2b47081ae4b22ddb22afd0c402ec0c8a7f646d
Nicholas <nicholasganshyan@gmail.com>
2019-01-07 23:01:17 +0800

    Migrate to AndroidX

--- app/src/main/java/com/example/user/swipeanddrag/Drag/DragRVTouchHelper.java
+++ app/src/main/java/com/example/user/swipeanddrag/Drag/DragRVTouchHelper.java
@@ -1,9 +1,9 @@
 package com.example.user.swipeanddrag.Drag;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.ItemTouchHelper;
 
 
 public class DragRVTouchHelper extends ItemTouchHelper.SimpleCallback {
--- app/src/main/java/com/example/user/swipeanddrag/MainActivity.java
+++ app/src/main/java/com/example/user/swipeanddrag/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.user.swipeanddrag;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
